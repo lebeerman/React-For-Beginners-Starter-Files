ab5d13f1faddc86e343078ebe41c89b99fb2c950
dan beerman <omnidbeerman@gmail.com>
2019-01-23 10:10:12 -0700

    Finished crouse, added notes

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import PropTypes from 'prop-types';
+import React from 'react';
+import PropTypes from "prop-types";
 import Header from "./Header";
-import Order from "./Order";
 import Inventory from "./Inventory";
+import Order from "./Order";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
 import base from "../base";
@@ -15,8 +15,8 @@ class App extends React.Component {
 
   static propTypes = {
     match: PropTypes.object
-  }
-
+  };
+  
   componentDidMount() {
     const { params } = this.props.match;
     // first reinstate our localStorage
@@ -38,69 +38,62 @@ class App extends React.Component {
       JSON.stringify(this.state.order)
     );
   }
-
-  componentWillUnmount() {
+  
+  componentWillUnmount(){
     base.removeBinding(this.ref);
   }
 
   addFish = fish => {
-    // 1. Take a copy of the existing state
+    // 1. take a copy of the existing state
+    // - no direct edits on state - this is a mutation... no good
     const fishes = { ...this.state.fishes };
-    // 2. Add our new fish to that fishes variable
-    fishes[`fish${Date.now()}`] = fish;
-    // 3. Set the new fishes object to state
+    fishes[`fish${Date.now()}`] = fish;    
     this.setState({ fishes });
-  };
+  }
 
   updateFish = (key, updatedFish) => {
-    // 1. Take a copy of the current state
     const fishes = { ...this.state.fishes };
-    // 2. Update that state
     fishes[key] = updatedFish;
-    // 3. Set that to state
     this.setState({ fishes });
-  };
+  }
 
-  deleteFish = key => {
-    // 1. take a copy of state
-    const fishes = { ...this.state.fishes };
-    // 2. update the state
+  deleteFish = (key) => {
+    // copy state, update state, set state
+    const fishes = { ...this.state.fishes }
     fishes[key] = null;
-    // 3.  update state
     this.setState({ fishes });
-  };
+  }
 
+  
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
-  };
+  }
 
-  addToOrder = key => {
-    // 1. take a copy of state
-    const order = { ...this.state.order };
-    // 2. Either add to the order, or update the number in our order
+  addToOrder = (key) => {
+    // 1 Copy state
+    const order = { ...this.state.order};
+    // add or update the number in the order
     order[key] = order[key] + 1 || 1;
-    // 3. Call setState to update our state object
+    // call setState to update our object
     this.setState({ order });
-  };
-
-  removeFromOrder = key => {
-    // 1. take a copy of state
-    const order = { ...this.state.order };
-    // 2. remove that itemf from order
+  }
+  
+  removeFromOrder = (key) => {
+    // copy state, update state, set state
+    const order = { ...this.state.order }
     delete order[key];
-    // 3. Call setState to update our state object
     this.setState({ order });
-  };
+  }
 
   render() {
-    return (
+    return ( 
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market" />
+          <Header tagline="Fresh Seafood Marketplace"/>   
           <ul className="fishes">
             {Object.keys(this.state.fishes).map(key => (
-              <Fish
-                key={key}
+              <Fish 
+                key={key} 
                 index={key}
                 details={this.state.fishes[key]}
                 addToOrder={this.addToOrder}
@@ -108,17 +101,18 @@ class App extends React.Component {
             ))}
           </ul>
         </div>
-        <Order
+        <Order 
           fishes={this.state.fishes}
           order={this.state.order}
           removeFromOrder={this.removeFromOrder}
-        />
-        <Inventory
-          addFish={this.addFish}
-          updateFish={this.updateFish}
+          {...this.state} />
+        <Inventory 
+          addFish={this.addFish} 
+          updateFish={this.updateFish} 
           deleteFish={this.deleteFish}
-          loadSampleFishes={this.loadSampleFishes}
-          fishes={this.state.fishes}
+          loadSampleFishes={this.loadSampleFishes} 
+          fishes={this.state.fishes} 
+          storeId={this.props.match.params.storeId}
         />
       </div>
     );
--- stepped-solutions/21/components/Inventory.js
+++ stepped-solutions/21/components/Inventory.js
@@ -1,12 +1,25 @@
 import React from "react";
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
-
+import Login from "./Login";
+import PropTypes from "prop-types";
 class Inventory extends React.Component {
+  static propTypes = {
+    fishes: PropTypes.object,
+    updateFish: PropTypes.func,
+    deleteFish:PropTypes.func,
+    loadSampleFishes:PropTypes.func,
+  }
+
+  authenticate = () => {
+    alert('NOPE');
+  }
+
   render() {
     return (
       <div className="inventory">
         <h2>Inventory</h2>
+        <Login authenticate={this.authenticate} />
         {Object.keys(this.props.fishes).map(key => (
           <EditFishForm
             key={key}
